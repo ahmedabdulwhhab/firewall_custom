dc2e49a86d3d70f70fd29e905a2c58da1fa7f743
ahmedabdulwhhab <80491399+ahmedabdulwhhab@users.noreply.github.com>
2021-10-27 09:03:05 +0200

    Update topo_005.py

--- topo_005.py
+++ topo_005.py
@@ -15,12 +15,12 @@ class RoutingTopo(Topo):
         s1  =self.addSwitch('s1', dpid='0000000000000001',cls=OVSSwitch, protocols='OpenFlow13')
         s2  =self.addSwitch('s2', dpid='0000000000000002',cls=OVSSwitch, protocols='OpenFlow13')
         s21 =self.addSwitch('s21',dpid='0000000000000021',cls=OVSSwitch, protocols='OpenFlow13')
-        h1= self.addHost('h1',mac="00:00:00:00:00:01",ip="192.168.1.201/24")
-        h2= self.addHost('h2',mac="00:00:00:00:00:02",ip="192.168.1.202/24")
-        h3= self.addHost('h3',mac="00:00:00:00:00:03",ip="192.168.1.203/24")
-        h4= self.addHost('h4',mac="00:00:00:00:00:04",ip="192.168.1.204/24")
-        h5= self.addHost('h5',mac="00:00:00:00:00:05",ip="192.168.1.205/24")
-        h6= self.addHost('h6',mac="00:00:00:00:00:06",ip="192.168.1.206/24")        
+        h1= self.addHost('h1',mac="00:00:00:00:00:01",ip="10.0.0.1/32")
+        h2= self.addHost('h2',mac="00:00:00:00:00:02",ip="10.0.0.2/32")
+        h3= self.addHost('h3',mac="00:00:00:00:00:03",ip="10.0.0.3/32")
+        h4= self.addHost('h4',mac="00:00:00:00:00:04",ip="10.0.0.4/32")
+        h5= self.addHost('h5',mac="00:00:00:00:00:05",ip="10.0.0.5/32")
+        h6= self.addHost('h6',mac="00:00:00:00:00:06",ip="10.0.0.6/32")        
 
         # Wire the switches and hosts together. Note there is a loop!
         self.addLink("h1", "s1", bw=20.0, delay='10ms', use_htb=True)
